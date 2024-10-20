90578ac7226d36514a8877ef77975d85e3f912c2
Юрий <urtets@gmail.com>
2024-10-20 09:51:33 +0300

    Homework Module_7

---
+++ practice_app/explorer.py
@@ -0,0 +1,16 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title('Проводник')
+window.geometry('350x350')
+window.configure(bg='black')
+window.resizable(False, False)
+text = tkinter.Label(window, text='Файл', height=10, width=20, background='silver')
+text.grid(column=1, row=1)
+button_select = tkinter.Button(window, width=20, height=3, text='Выбрать файл')
+button_select.grid(column=1, row=2)
+button_select.configure(bg='blue', fg='red')
+
+
+
+window.mainloop()
