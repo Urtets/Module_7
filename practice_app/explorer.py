c68ef96f87c6d2aa5dc733a5547dd4d4e9229799
Юрий <urtets@gmail.com>
2024-10-21 11:38:08 +0300

    Homework Module_7

--- practice_app/explorer.py
+++ practice_app/explorer.py
@@ -1,15 +1,24 @@
 import tkinter
+import os
+from tkinter import filedialog
+
+def file_select():
+    filename = filedialog.askopenfilename(initialdir="/", title="Выберите файл",
+                                          filetypes=(('Текстовый файл', '.txt'),
+                                                     ('Все файлы', '*')))
+    text['text'] = text['text'] + ' ' + filename
+    os.startfile(filename)
 
 window = tkinter.Tk()
 window.title('Проводник')
-window.geometry('350x350')
-window.configure(bg='black')
+window.geometry('500x200')
+window.configure(bg='grey')
 window.resizable(False, False)
-text = tkinter.Label(window, text='Файл', height=10, width=20, background='silver')
+text = tkinter.Label(window, text='Файл', height=8, width=70, background='silver')
 text.grid(column=1, row=1)
-button_select = tkinter.Button(window, width=20, height=3, text='Выбрать файл')
-button_select.grid(column=1, row=2)
-button_select.configure(bg='blue', fg='red')
+button_select = tkinter.Button(window, width=20, height=3, text='Выбрать файл', command=file_select)
+button_select.grid(column=1, row=2, pady=5)
+button_select.configure(bg='yellow', fg='green')
 
 
 
